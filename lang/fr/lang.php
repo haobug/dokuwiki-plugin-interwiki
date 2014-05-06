6b6f135c0b06b0877d820ab7c09be6e88ba02997
Nicolas Friedli <nicolas@theologique.ch>
2014-05-06 14:55:37 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['qb_interwiki']          = 'Insérer un lien interwiki';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['excluded_shortcuts']    = 'Raccouris interwiki exclus';
