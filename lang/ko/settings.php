37aa96c21ec3346c215453c1769ab81ea55fa549
Myeongjin <aranet100@gmail.com>
2016-07-02 17:16:59 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Korean language file for interwiki plugin
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['excluded_shortcuts'] = '제외되는 인터위키 바로 가기';
+
+
+
+//Setup VIM: ex: et ts=4 :
