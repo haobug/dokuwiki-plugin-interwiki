9a7c2eb1e0d3f6d37434518b3f593ac505e5e1eb
Tor Härnqvist <tor@harnqvist.se>
2018-01-18 11:00:34 +0100

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['excluded_shortcuts']    = 'Interwiki-genvägar exkluderade';
