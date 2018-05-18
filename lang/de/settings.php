11cfac7439d96bd4d8975d6f696a1fdd1d947739
Sebastian Engel <mail@engel-s.de>
2018-05-18 13:00:34 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sebastian Engel <mail@engel-s.de>
+ */
+$lang['excluded_shortcuts']    = 'Interwiki Kürzel ausgenommen';
