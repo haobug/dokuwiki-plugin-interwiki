4ed7e9c02df021c34232bc81f6e88d9b4eba9055
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 14:40:05 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['qb_interwiki']          = 'Insira o link interwiki';
